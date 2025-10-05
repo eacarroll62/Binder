fdf4b815a2d07ec1030e8e4d638b10dd4ab37f0e
eacarroll62 <peacarroll@comcast.net>
2025-10-05 15:23:21 -0400

    Initial Commit

---
+++ BinderTests/BinderTests.swift
@@ -0,0 +1,17 @@
+//
+//  BinderTests.swift
+//  BinderTests
+//
+//  Created by Eric Carroll on 10/5/25.
+//
+
+import Testing
+@testable import Binder
+
+struct BinderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
